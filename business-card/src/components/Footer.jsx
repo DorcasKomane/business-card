1fbb8fb240494b1928da61e28e559064a7d2f2f2
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2022-09-23 16:32:11 +0200

    added Info, About and Interests components.

---
+++ business-card/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer(){
+
+    <div>
+        <i class="fa-brands fa-twitter" id="twitter"></i>
+
+    </div>
+
+}
---
+++ business-card/src/components/Interests.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Interests(){
+    return (
+        <div>
+            <h3 className="section-header">Interests</h3>
+            <p className="interests--p">Music Lover. Jazz, pop, and everything in-between. 
+            Novice guitar player. Reader.</p>
+        </div>
+    )
+}
